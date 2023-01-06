ae4c37284a8f25cf9e5bfc8d5d3036164673cb04
Sanket Shahane <shahanesanket24@gmail.com>
2023-01-05 17:31:05 -0800

    spring boot backend structure added

---
+++ Backend Server/roomity/src/main/java/com/roomity/RoomityApplication.java
@@ -0,0 +1,13 @@
+package com.roomity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoomityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoomityApplication.class, args);
+	}
+
+}
---
+++ Backend Server/roomity/src/test/java/com/roomity/RoomityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.roomity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoomityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
